27955ee26ca2d10897a8fe92a2f3c12bf499a599
miguel-wh <miguel@wisphub.net>
2024-05-29 17:56:24 -0500

    Initial

---
+++ operaciones/tests.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class OperacionesTests(TestCase):
+    """
+    def test_sumar(self):
+        response = self.client.get(reverse('sumar'), {'a': 1, 'b': "d"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'resultado': 3})
+
+    def test_restar(self):
+        response = self.client.get(reverse('restar'), {'a': 5, 'b': 3})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'resultado': 2})
+
+    def test_multiplicar(self):
+        response = self.client.get(reverse('multiplicar'), {'a': 2, 'b': 3})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'resultado': 6})
+
+    def test_dividir(self):
+        response = self.client.get(reverse('dividir'), {'a': 10, 'b': 2})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'resultado': 5})
+
+        response = self.client.get(reverse('dividir'), {'a': 10, 'b': 0})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'error': 'No se puede dividir por cero'})
+    
+    """
+    def test_par(self):
+        response = self.client.get(reverse('detectar_par'), {'a': "d"})
+        self.assertEqual(response.json(), {'resultado': False})
+
---
+++ operaciones/views.py
@@ -0,0 +1,40 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'operaciones/home.html')
+
+
+def detectar_par(request):
+    par = request.GET.get('a')
+    resultado = False
+    if int(par) % 2 == 0:
+        resultado = True
+    return JsonResponse({'resultado': resultado})
+
+
+def sumar(request):
+    a = float(request.GET.get('a', 0))
+    b = float(request.GET.get('b', 0))
+    resultado = a + b
+    return JsonResponse({'resultado': resultado})
+
+def restar(request):
+    a = float(request.GET.get('a', 0))
+    b = float(request.GET.get('b', 0))
+    resultado = a - b
+    return JsonResponse({'resultado': resultado})
+
+def multiplicar(request):
+    a = float(request.GET.get('a', 0))
+    b = float(request.GET.get('b', 0))
+    resultado = a * b
+    return JsonResponse({'resultado': resultado})
+
+def dividir(request):
+    a = float(request.GET.get('a', 0))
+    b = float(request.GET.get('b', 1))  # Para evitar división por cero por defecto es 1
+    if b == 0:
+        return JsonResponse({'error': 'No se puede dividir por cero'}, status=400)
+    resultado = a / b
+    return JsonResponse({'resultado': resultado})

ac4896dc0d3417686ea806a331eb011f8ad1a2aa
miguel-wh <miguel@wisphub.net>
2024-05-31 00:56:04 -0500

    Test caso invalido: no es un numero funcion es par

--- operaciones/tests.py
+++ operaciones/tests.py
@@ -23,5 +23,31 @@ class OperacionesTests(TestCase):
 
     def test_es_par(self):
         response = self.client.get(reverse('detectar_par'), {'a': "2"})
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {'resultado': True})
+
+        # Caso válido: número impar
+        response = self.client.get(reverse('detectar_par'), {'a': "3"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'resultado': False})
+
+        # Caso válido: número impar
+        response = self.client.get(reverse('detectar_par'), {'a': "3"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'resultado': False})
+
+        # Caso inválido: no es un número
+        response = self.client.get(reverse('detectar_par'), {'a': "dos"})
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'error': 'El parámetro debe ser un número entero'})
+
+        # # Caso inválido: parámetro vacío
+        # response = self.client.get(reverse('detectar_par'), {'a': ""})
+        # self.assertEqual(response.status_code, 400)
+        # self.assertEqual(response.json(), {'error': 'El parámetro debe ser un número entero'})
+        #
+        # # Caso inválido: parámetro no presente
+        # response = self.client.get(reverse('detectar_par'))
+        # self.assertEqual(response.status_code, 400)
+        # self.assertEqual(response.json(), {'error': 'El parámetro debe ser un número entero'})
 

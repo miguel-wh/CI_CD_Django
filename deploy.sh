4725235cfac16a82062044273c63f946b1cdf4e8
miguel-wh <miguel@wisphub.net>
2024-05-31 16:53:08 -0500

    logs deploy

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,25 @@
 # Navegar al directorio de tu proyecto
 cd ~/cd_django/
 
-echo "*******git pull******"
+echo "*******Inicia - git pull******"
 # Actualizar el repositorio
 git pull origin master
-echo "*******end git pull******"
+echo "*******Termina - git pull******"
+
 # Activar el entorno virtual
 source ~/cd/bin/activate
 
 # Instalar dependencias
+echo "*******Inicia - Instalar dependencias******"
 pip install -r requirements.txt
+echo "*******Termina - Instalar dependencias******"
 
 # Aplicar migraciones de la base de datos
+echo "*******Inicia - Aplicar migraciones******"
 python manage.py migrate
+echo "*******Inicia - Aplicar migraciones******"
 
+echo "*******Inicia - Reiniciar servicio******"
+echo "*******Termina - Reiniciar servicio******"
 # Recopilar archivos estáticos
 #python manage.py collectstatic --noinput
